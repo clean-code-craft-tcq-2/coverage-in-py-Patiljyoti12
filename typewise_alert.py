f6fe18e0a17536bc1d4dcd3095bf2f6aaef1b651
Patiljyoti12 <59950788+Patiljyoti12@users.noreply.github.com>
2022-02-20 15:55:35 +0530

    Update typewise_alert.py

--- typewise_alert.py
+++ typewise_alert.py
@@ -18,7 +18,7 @@ def classify_temperature_breach(value,coolingType):
     
 
 
-def check_and_alert(alertTarget,breachType)
+def check_and_alert(alertTarget,breachType):
   if alertTarget == 'TO_CONTROLLER':
     send_to_controller(breachType)
   elif alertTarget == 'TO_EMAIL':
